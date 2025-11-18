2d01b1a587f85ad2485cdac15473dafc4810cd83
xmr-gif <hafidoss2004@gmail.com>
2025-11-18 23:30:04 +0100

    create overview component

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Sidebar} from '../../shared/sidebar/sidebar';
 import {TicketsBoard} from '../tickets/components/tickets-board/tickets-board';
+import {Overview} from './overview/overview';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     Sidebar,
-    TicketsBoard
+    TicketsBoard,
+    Overview
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
