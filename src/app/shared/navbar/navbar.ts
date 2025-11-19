bcef8053b64a5ed7bbff95062b09fb2bda8d3f48
tutankhamun-prog <nikelmohamedtaha@gmail.com>
2025-11-19 15:33:20 +0100

    create navbar compo

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
+
+interface NavItem {
+  icon: string;
+  label: string;
+  active?: boolean;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,
+    LucideAngularModule
+  ],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class NavbarComponent {
+  isProjectDropdownOpen = false;
+  isProfileDropdownOpen = false;
+  hasNotifications = true;
+
+  projects = [
+    { id: 1, name: 'TGO5' },
+    { id: 2, name: 'Symfony' },
+    { id: 3, name: '7douti' },
+    { id: 4, name: 'Mani' }
+  ];
+
+  selectedProject = this.projects[0];
+
+  toggleProjectDropdown() {
+    this.isProjectDropdownOpen = !this.isProjectDropdownOpen;
+    if (this.isProjectDropdownOpen) {
+      this.isProfileDropdownOpen = false;
+    }
+  }
+
+  isAppsDropdownOpen = false;
+
+toggleAppsDropdown() {
+  this.isAppsDropdownOpen = !this.isAppsDropdownOpen;
+  this.isProjectDropdownOpen = false;
+  this.isProfileDropdownOpen = false;
+}
+
+
+  toggleProfileDropdown() {
+    this.isAppsDropdownOpen =false;
+    this.isProfileDropdownOpen = !this.isProfileDropdownOpen;
+    if (this.isProfileDropdownOpen) {
+      this.isProjectDropdownOpen = false;
+    }
+  }
+
+  selectProject(project: any) {
+    this.selectedProject = project;
+    this.isProjectDropdownOpen = false;
+  }
+
+  closeDropdowns() {
+    this.isProjectDropdownOpen = false;
+    this.isProfileDropdownOpen = false;
+    this.isAppsDropdownOpen = false;
+  }
 
 }
