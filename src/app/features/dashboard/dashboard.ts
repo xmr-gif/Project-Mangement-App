d091f18d8805d61403ac6fba31603a3e7071b794
chvr4f <charaf2322004@gmail.com>
2025-11-19 16:46:16 +0100

    added navbar to dashboard.html

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {Sidebar} from '../../shared/sidebar/sidebar';
 import {TicketsBoard} from '../tickets/components/tickets-board/tickets-board';
 import {Overview} from './overview/overview';
+import {NavbarComponent} from '../../shared/navbar/navbar';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     Sidebar,
     TicketsBoard,
-    Overview
+    Overview,
+    NavbarComponent
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
