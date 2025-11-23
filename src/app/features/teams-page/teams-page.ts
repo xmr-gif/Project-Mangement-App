e799571f6bd332b9bb026d49315d77a37d4f1407
Youssef Kmimech <kmimechyoussef@gmail.com>
2025-11-23 13:06:36 +0100

    Création du composant teams-page

---
+++ src/app/features/teams-page/teams-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams-page',
+  imports: [],
+  templateUrl: './teams-page.html',
+  styleUrl: './teams-page.css',
+})
+export class TeamsPage {
+
+}
